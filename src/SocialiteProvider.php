d0380d4a6bfed65e231442718d893d7b35eb845d
tectiv3 <git@getpanda.me>
2019-10-17 15:18:59 +0900

    Update socialite provider
    Add ability to retrieve first and last name

--- src/SocialiteProvider.php
+++ src/SocialiteProvider.php
@@ -5,10 +5,10 @@ namespace SeteMares\Apple;
 use Illuminate\Support\Arr;
 use Laravel\Socialite\Two\AbstractProvider;
 use Laravel\Socialite\Two\User;
-use GuzzleHttp\ClientInterface;
 use Laravel\Socialite\Two\InvalidStateException;
+use Laravel\Socialite\Two\ProviderInterface;
 
-class SocialiteProvider extends AbstractProvider
+class SocialiteProvider extends AbstractProvider implements ProviderInterface
 {
     /**
      * {@inheritdoc}
@@ -26,12 +26,34 @@ class SocialiteProvider extends AbstractProvider
     /**
      * {@inheritdoc}
      */
+    protected $encodingType = PHP_QUERY_RFC3986;
+
     protected function getAuthUrl($state)
     {
-        return $this->with(['response_mode' => 'form_post'])->buildAuthUrlFromBase(
+        return $this->buildAuthUrlFromBase(
             'https://appleid.apple.com/auth/authorize',
             $state
         );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCodeFields($state = null)
+    {
+        $fields = [
+            'client_id' => $this->clientId,
+            'redirect_uri' => $this->redirectUrl,
+            'scope' => $this->formatScopes($this->getScopes(), $this->scopeSeparator),
+            'response_type' => 'code',
+            'response_mode' => 'form_post',
+        ];
+
+        if ($this->usesState()) {
+            $fields['state'] = $state;
+        }
+
+        return array_merge($fields, $this->parameters);
     }
 
     /**
@@ -45,36 +67,32 @@ class SocialiteProvider extends AbstractProvider
     /**
      * {@inheritdoc}
      */
-    public function getAccessTokenResponse($code)
+    public function getAccessToken($code)
     {
-        $response = parent::getAccessTokenResponse($code);
-        $id_tokens = explode('.', Arr::get($response, 'id_token'));
+        $response = $this->getHttpClient()
+            ->post(
+                $this->getTokenUrl(),
+                [
+                    'headers' => [
+                        'Authorization' => 'Basic ' . base64_encode(
+                            $this->clientId . ':' . $this->clientSecret
+                        ),
+                    ],
+                    'body' => $this->getTokenFields($code),
+                ]
+            );
 
-        return array_merge(
-            $response,
-            [
-                'id_token_header' => json_decode(base64_decode(Arr::get($id_tokens, 0)), true),
-                'id_token_payload' => json_decode(base64_decode(Arr::get($id_tokens, 1)), true),
-                'id_token_signature' => Arr::get($id_tokens, 2),
-            ]
-        );
+        return $this->parseAccessToken($response->getBody());
     }
 
     /**
      * {@inheritdoc}
      */
-    public function user()
+    protected function parseAccessToken($response)
     {
-        if ($this->hasInvalidState()) {
-            throw new InvalidStateException();
-        }
+        $data = $response->json();
 
-        $response = $this->getAccessTokenResponse($this->getCode());
-        $user = $this->mapUserToObject($response);
-
-        return $user->setToken(Arr::get($response, 'access_token'))
-            ->setRefreshToken(Arr::get($response, 'refresh_token'))
-            ->setExpiresIn(Arr::get($response, 'expires_in'));
+        return $data['access_token'];
     }
 
     /**
@@ -82,28 +100,9 @@ class SocialiteProvider extends AbstractProvider
      */
     protected function getUserByToken($token)
     {
-        // $response = $this->getHttpClient()->get(
-        //     'https://appleid.apple.com/users/me',
-        //     [
-        //         'headers' => [
-        //             'Authorization' => 'Bearer ' . $token
-        //         ]
-        //     ]
-        // );
-        // return json_decode($response->getBody()->getContents(), true);
-        return [];
-    }
+        $claims = explode('.', $token)[1];
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function mapUserToObject(array $user)
-    {
-        return (new User())->setRaw($user)->map([
-            'id' => Arr::get($user, 'id_token_payload.sub'),
-            'name' => Arr::get($user, 'id_token_payload.name'),
-            'email' => Arr::get($user, 'id_token_payload.email'),
-        ]);
+        return json_decode(base64_decode($claims), true);
     }
 
     /**
@@ -114,5 +113,51 @@ class SocialiteProvider extends AbstractProvider
         return array_merge(parent::getTokenFields($code), [
             'grant_type' => 'authorization_code',
         ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function user()
+    {
+        if ($this->hasInvalidState()) {
+            throw new InvalidStateException;
+        }
+
+        $response = $this->getAccessTokenResponse($this->getCode());
+
+        $user = $this->mapUserToObject($this->getUserByToken(
+            Arr::get($response, 'id_token')
+        ));
+
+        return $user
+            ->setToken(Arr::get($response, 'access_token'))
+            ->setRefreshToken(Arr::get($response, 'refresh_token'))
+            ->setExpiresIn(Arr::get($response, 'expires_in'));
+    }
+
+    protected function mapUserToObject(array $user)
+    {
+        if (
+            request()->filled("user")
+            && array_key_exists("name", $user)
+        ) {
+            $user["name"] = Arr::get(json_decode(request("user"), true), 'name');
+            $user['first_name'] = Arr::get($user, 'name.firstName');
+            $user['last_name'] = Arr::get($user, 'name.lastName');
+            $fullName = trim(
+                ($user['first_name'] ?: "")
+                    . " "
+                    . ($user['last_name'] ?: "")
+            );
+        }
+
+        return (new User)
+            ->setRaw($user)
+            ->map([
+                "id" => $user["sub"],
+                "name" => $fullName ?? null,
+                "email" => $user["email"] ?? null,
+            ]);
     }
 }
